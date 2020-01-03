fc6d89ab1ca92ccb6fce96d6ed123c29efd80249
Blendi Ibraj <blendi.ibraj96@gmail.com>
2020-01-03 14:51:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
 // add solution here
-function theBeatlesPlay(players, instruments) {
-  var allPlayers = [];
-  for (var i=0; i<players.length; i++) {
-    allPlayers.push(players[i] + "plays" + instruments[i]);
-  }
-  return allPlayers;
-}
